33b51f56ae76f5286980a24eaedae212d6ee478e
Maria Melnikova <mariarockrockrock@rock.rock>
2023-04-21 01:21:32 +0300

    added css and html

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_masha():
-    return "<p>Hello, Rock</p>"
+    return render_template('home.html') 
 
 
 if __name__ == "__main__":
