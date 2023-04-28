0b06e3e9bb62f969a64a9622683a773834a94a1e
Maria Melnikova <mariarockrockrock@rock.rock>
2023-04-28 08:29:32 +0300

    Tried to add some interactivity

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
-from flask import Flask, render_template, jsonify 
+from flask import Flask, render_template, jsonify, request
+from werkzeug.security import generate_password_hash
+
+from . import db
+
 
 app = Flask(__name__)
+
+db.init_app(app)
 
 MOVIES = [
     {
@@ -35,12 +41,21 @@ LOL = [{'lol laugh'}]
 def hello_masha():
     return render_template('home.html', 
                           movies=MOVIES,
-                          theater_location='Turku', 
-                          lol=LOL) 
+                          theater_location='Turku',) 
 
 @app.route("/api/movies")
 def list_movies():
-    return  jsonify(MOVIES)
+    return jsonify(MOVIES)
+
+@app.route('/submit', methods=['POST', 'GET'])
+def show_data():
+    database = db.get_db()
+    
+    database.execute(
+        "INSERT INTO user (username, password) VALUES (?, ?)",
+        (request.args['fname'], generate_password_hash(request.args['fname'])),
+    )
+    return jsonify({'fname': request.args['fname']})
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
